6de280fc9640487c408f045b44c8c1825cec0756
react220 <react@branderstudio.com>
2023-03-04 17:12:21 +0100

    add method

---
+++ example/Factorial.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Factorial {
+    public long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
